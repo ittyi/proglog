f851108d931e158ce6b184703d12594a13d2b5d5
ittyi <vioiv.k4@gmail.com>
2023-10-03 03:59:38 +0900

    🐛 1.4 add: Produce&ConsumeのRequestとResponse構造体

--- internal/server/http.go
+++ internal/server/http.go
@@ -25,3 +25,19 @@ func newhttpServer() *httpServer {
 		Log: NewLog(),
 	}
 }
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
