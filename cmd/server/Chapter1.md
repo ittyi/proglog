e61752c5902d9aeb7750f31e54dedb72b8f0de29
ittyi <vioiv.k4@gmail.com>
2023-10-06 01:51:35 +0900

    📝 handleProduceの挙動をある程度理解

--- cmd/server/Chapter1.md
+++ cmd/server/Chapter1.md
@@ -44,3 +44,11 @@ curl -X POST localhost:8080 -d \
 {"offset":2}
 
 ```
+
+
+GETもできる
+```
+curl -X GET localhost:8080 -d \
+'{"record": {"value": "TGV0J3MgR28gIzEK"}}'
+{"record":{"value":"TGV0J3MgR28gIzEK","offset":0}}
+```
--- internal/server/http.go
+++ internal/server/http.go
@@ -69,6 +69,21 @@ func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var req ProduceRequest
+	// Body はリクエストの本体です。
+	//
+	// クライアントリクエストの場合、nil ボディはリクエストに何も含まれていないことを意味します。
+	// 本文（GET リクエストなど）。 HTTP クライアントのトランスポート
+	// は Close メソッドを呼び出す役割を果たします。
+	//
+	// サーバーリクエストの場合、リクエストボディは常に非 nil です
+	// ただし、ボディが存在しない場合はすぐに EOF を返します。
+	// サーバーはリクエスト本文を閉じます。 ServeHTTP
+	// ハンドラーはその必要はありません。
+	//
+	// 本体では、Close と同時に Read を呼び出すことができる必要があります。
+	// 特に、Close を呼び出すと、待機中の読み取りのブロックが解除されるはずです
+	// 入力用。
+	println("r.Body:", r.Body)
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -80,8 +95,10 @@ func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	println("off:", off)
 
 	res := ProduceResponse{Offset: off}
+	println("res.Offset:", res.Offset)
 	err = json.NewEncoder(w).Encode(res)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
